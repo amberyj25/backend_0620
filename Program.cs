a6037da3605967235922803087e4c3a8a6e3927c
amberyj25 <amber20240@gmail.com>
2023-06-20 23:17:05 +0800

    CORS backend

--- Program.cs
+++ Program.cs
@@ -8,12 +8,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+// 中介軟體配置
+app.UseRouting();
+// 允許所有來源的跨域請求
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+// 路由配置
+app.UseEndpoints(endpoints =>
+{
+  endpoints.MapControllers();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
