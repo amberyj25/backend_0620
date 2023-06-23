ac7fc58b7a3aff9136ba2c9867f8d0189d9fa52c
amberyj25 <amber20240@gmail.com>
2023-06-23 12:53:15 +0800

    新增一隻API

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherController> _logger;
+
+    public WeatherController(ILogger<WeatherController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet(Name = "GetWeather")]
+    public IEnumerable<Weather> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Weather
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
